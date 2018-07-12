297c53cfbca75ac18b93f1c38a05353cf4ebb147
김민주 <kimminju@gimminjuui-MacBook-Pro.local>
2018-07-12 21:39:35 +0900

    Attach Facebook login SDK

--- client/js/App.js
+++ client/js/App.js
@@ -1,5 +1,12 @@
 export default class App {
     constructor() {
-        console.log("HELLO WORLD!")
+        console.log("HELLO WORLD!");
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v3.0&appId=217172708910978&autoLogAppEvents=1';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     }
 }
