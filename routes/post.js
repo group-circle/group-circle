a45acf614f19c37f14dbde9d67af4517b1e1e736
astrod <cha452jg@gmail.com>
2018-07-20 02:02:45 +0900

    make post API

--- routes/post.js
+++ routes/post.js
@@ -6,10 +6,10 @@ let postRepository= require("../src/PostRepository");
 
 /**
  * 포스트 목록을 반환한다.
- * category 가 undefined 인 경우는 없고, 최초호출시 lastPostId 는 undefined 일수 있다.
+ * category 가 undefined 인 경우는 없고, 최초호출시 lastKey 는 undefined 일수 있다.
  */
 router.get('/', async function(req, res, next) {
-    const lastPostId = req.query.lastPostId
+    const lastKey = req.query.lastKey
     let category = req.query.category
 
     if(typeof category === 'undefined') {
@@ -19,21 +19,49 @@ router.get('/', async function(req, res, next) {
 
     category = Number.parseInt(category)
 
-    if(typeof lastPostId === 'undefined') {
+    if(typeof lastKey === 'undefined') {
         const r = await postRepository.findAllPostList(category)
-        res.send(r.val())
+        res.send(toResponse(r))
         return
     }
 
-    const r = await postRepository.findPostListByLastPostIdAndCategory(lastPostId, category)
-    res.send(r.val())
+    const r = await postRepository.findPostListByLastKeyAndCategory(lastKey, category)
+    res.send(toResponse(r))
 });
 
+const toResponse = (r) => {
+    const result = {}
+    r.forEach((each) => {
+        result[each.key] = each.val()
+    })
+
+    if(Object.keys(result).length === 0 && result.constructor === Object) {
+        return {
+            "list" : null,
+            "lastKey" : null
+        }
+    }
+
+    return {
+        "list" : result,
+        "lastKey" : makeLastKey(result)
+    }
+}
+
+const makeLastKey = (result) => {
+    if(Object.keys(result).length < 20) {
+        return null;
+    }
+
+    return Object.keys(result)[Object.keys(result).length - 1]
+}
+
 /**
- * 더미 데이터를 insert 한다. 데이터는 총 50개이다.
+ * 더미 데이터를 insert 한다.
  */
 router.get('/sample', function (req, res) {
     postRepository.addDummyPostList();
+    res.send("success")
 });
 
 module.exports = router;
--- src/PostRepository.js
+++ src/PostRepository.js
@@ -1,64 +1,47 @@
 const db = require("./Firebase");
+const LIMIT = 20;
+const categories = require("./Category")
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 module.exports = {
-    DEFAULT_DATA_PATH : 'contents',
-    ref : db.ref(this.DEFAULT_DATA_PATH).child('posts'),
-    LIMIT : 20,
 
-    add : function(postId, data) {
-        if(Number.isInteger(postId)) {
-            postId = postId.toString();
-        }
-
-        const postRef = this.ref.child(postId);
-        postRef.set(data);
-    },
-
+    ref : db.ref('contents/posts'),
     /**
      * 더미 데이터 만드는 용도로 사용
      * 실제 컨트롤러에서는 호출하지 말 것
      */
     addDummyPostList : function () {
-        for(let i = 1; i<51; i++) {
-            const key = Number.MAX_SAFE_INTEGER - i;
+        for(let i = 1; i<101; i++) {
+            const now = new Date().getTime();
             let data = {
-                postId: i,
-                url: "http://www.daum.com",
-                createdAt: new Date().getTime(),
-                category: getRandomInt(0, 27)
+                url : "http://www.daum.com",
+                createdAt : now,
+                category : getRandomInt(0, 27)
             }
 
-            const postRef = this.ref.child(key.toString())
-            postRef.set(data);
+            this.ref.child(Number.MAX_SAFE_INTEGER - now).set(data)
         }
     },
 
-    get : function() {
-        return this.ref.once('value');
-    },
-    
-    findPostListByLastPostIdAndCategory : function (lastPostId, category) {
+    findPostListByLastKeyAndCategory : function (lastKey, category) {
 
-        const key = (Number.MAX_SAFE_INTEGER - lastPostId).toString()
-
-        if(category === 0) {
-            console.log(key);
-            return this.ref.orderByChild("postId").startAt(lastPostId).once("value")
+        if(category === categories["all"]) {
+            return this.ref.orderByKey().startAt(lastKey).limitToFirst(LIMIT).once("value")
         }
 
-        return this.ref.orderByChild('category').equalTo(category).orderByValue().startAt(key).limitToFirst(this.LIMIT).once("value")
+        return this.ref.orderByChild('category').equalTo(category).startAt(lastKey).limitToFirst(LIMIT).once("value")
     },
 
     findAllPostList : function(category) {
-        if(category === 0) {
-            return this.ref.orderByValue().limitToFirst(this.LIMIT).once("value")
+
+        if(category === categories["all"]) {
+            return this.ref.orderByKey().limitToFirst(LIMIT).once("value")
         }
 
-        return this.ref.orderByChild("category").equalTo(category).limitToFirst(this.LIMIT).once("value")
+        return this.ref.orderByChild("category").equalTo(category).limitToFirst(LIMIT).once("value")
     }
 };
 
