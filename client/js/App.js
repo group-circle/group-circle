4f878664bef982e7905f30a7af1b70847d46c790
김민주 <kimminju@gimminjuui-MacBook-Pro.local>
2018-07-22 14:26:02 +0900

    code clean-up

--- client/js/App.js
+++ client/js/App.js
@@ -1,11 +1,14 @@
 export default class App {
-    constructor() {
+    constructor() {;
+    }
+
+    attachFBLogin() {
         (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
             js = d.createElement(s); js.id = id;
             js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v3.0&appId=217172708910978&autoLogAppEvents=1';
             fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        }(document, 'script', 'facebook-jssdk'))
     }
-}
+}``
