064540792e740bfbf83b38e0a3c829eb9d4a2a0a
김민주 <kimminju@gimminjuui-MacBook-Pro.local>
2018-07-24 19:14:57 +0900

    share to Facebook

--- client/js/index.js
+++ client/js/index.js
@@ -20,6 +20,11 @@ window.checkFBLogin = () => {
     loginCheck();
 }
 
+window.openNewWindow = (dom) => {
+    let url = $(dom).data('url');
+    window.open(url,'Share to Facebook','width=550,height=500');
+}
+
 function loginCheck () {
     FB.getLoginStatus((response) => {
         switch (response.status) {
@@ -59,12 +64,14 @@ function drawList(category, lastKey) {
         let lists = json.list;
         let listHtml = '';
         let postTemplate = _.template('<div class="list" data-category=<%= category %>><span class="url"> <%= url %> </span><img class="img" src=<%= image %>>' +
-          '<div class="domain"><%= domain %></div><div class="title"><%= title %></div><div class="desc"><%= description %></div></div>');
+          '<div class="domain"><%= domain %></div><div class="title"><%= title %></div><div class="desc"><%= description %></div>' +
+          '<div class="fb-share-button" data-href=<%= url %> data-layout="button_count" data-size="small" data-mobile-iframe="true"><a href="javascript:;" onClick="openNewWindow(this)" data-url=<%= shareUrl %> class="fb-xfbml-parse-ignore">공유하기</a></div></div>');
 
         $.each(lists, (key, item) => {
             let meta = item.metadata;
             meta.category = item.category;
             meta.domain = item.url.split('://')[1].split('/')[0];
+            meta.shareUrl = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(item.url) + '&amp;src=sdkpreparse'
             listHtml += postTemplate(meta);
         });
         if (lastKey) {
